2f6b7cf9b61152ad4967510c4d3f2317a0545e6b
Mihail Ivanchev <contact@ivanchev.net>
2020-04-30 14:46:04 +0200

    Case-sensitive env var.

--- src/code_challenge/core.clj
+++ src/code_challenge/core.clj
@@ -40,5 +40,5 @@
     wrap-json-response))
 
 (defn -main [& args]
-  (let [port (Integer/valueOf (or (System/getenv "port") "3000"))]
+  (let [port (Integer/valueOf (or (System/getenv "PORT") "3000"))]
     (jetty/run-jetty app {:port port})))
